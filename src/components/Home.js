d3c3b1faa897e095cbf07e451d023a1c1f8ea5bc
said shah <saidmysr20@aol.com>
2019-02-18 19:51:08 -0500

    finished home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import your_carrer from '../images/your_career_your_choice.png'
 import your_track from '../images/Track.png'
+import job_poster from '../images/job_placement.png'
+import salary_poster from '../images/salary.png'
+import debug_poster from '../images/debug.png'
+import learn_poster from '../images/learn_by_building.png'
 
 class Home extends Component {
 
@@ -9,6 +13,13 @@ class Home extends Component {
       <div>
         <img src={your_carrer} alt="your career"/>
         <img src={your_track} alt="your track"/>
+        <img src={learn_poster} alt="learn poster"/>
+        <img src={job_poster} alt="job placement"/>
+        <img src={salary_poster} alt="salary poster"/>
+        <div className="text-center top-margin">
+        <a href="/apply" ><button className="btn blue">Apply Now</button></a>
+        </div>
+          <img src={debug_poster} alt="debug poster"/>
       </div>
     );
   }
